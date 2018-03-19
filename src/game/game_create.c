e766bf277267de3f95fbcc5684a2d0c0b33d811f
hadi-ilies.bereksi-reguig <hadi-ilies.bereksi-reguig@epitech.eu>
2018-03-19 14:10:26 +0100

    insert into map

--- src/game/game_create.c
+++ src/game/game_create.c
@@ -9,14 +9,30 @@
 #include "game.h"
 #include "my.h"
 
+char **my_realloc_2d(char **map, char *line)
+{
+	size_t i = 0;
+	char **new_map;
+
+	for (; map && map[i] != NULL; i++);
+	if ((new_map = malloc(sizeof(char *) * (i + 2))) == NULL)
+		return (NULL);
+	for (i = 0; map && map[i] != NULL; i++)
+		new_map[i] = map[i];
+	new_map[i] = line;
+	new_map[i + 1] = NULL;
+	free(map);
+	return (new_map);
+}
+
 char **save_file()
 {
 	char *str = get_next_line(0);
-	char **file;
+	char **file = NULL;
 
 	for (int i = 0; str != NULL; i++) {
-		file = malloc(sizeof(char *) * )
-
+		file = my_realloc_2d(file, str);
+		str = get_next_line(0);
 	}
 		return (file);
 }
@@ -26,10 +42,7 @@ game_t game_create(void)
 	game_t game;
 	char **file = save_file();
 
-	//game.room = NULL;
-	//if (game.nb_ant = atoi(get_next_line(0)) <= 0)
-	//	return (game);
-	//if (take_starter(&game) == false)
-	//	return (game);
+	for (int i = 0; file[i] != NULL; i++)
+		printf("%s\n", file[i]);
 	return (game);
 }
