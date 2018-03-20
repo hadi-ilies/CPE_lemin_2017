7ee24959f8342527fcba38843b7acbdb4ec50dd2
corentin.gautier <corentin.gautier@localhost.localdomain>
2018-03-20 09:20:48 +0100

    add main.c

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2017
+** lemin
+** File description:
+** main.c
+*/
+
+#include "prototype.h"
+
+int main(int nb)
+{
+	game_t game = game_create();
+
+	if (game.room == NULL)
+		return (84);
+	return (lemin(&game));
+}
