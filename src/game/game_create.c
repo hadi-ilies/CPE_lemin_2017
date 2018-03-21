1957cd5146e71f2e63023bde47f9fcacf02536bb
hadi-ilies.bereksi-reguig <hadi-ilies.bereksi-reguig@epitech.eu>
2018-03-21 19:19:30 +0100

    linked_list all rooms is linked

--- src/game/game_create.c
+++ src/game/game_create.c
@@ -103,7 +103,7 @@ bool check_room_exist(game_t *game, char **link)
 	return (true);
 }
 
-bool take_link(game_t *game, char **file)
+bool parsing3(game_t *game, char **file)
 {
 	char **line;
 	char **link;
@@ -112,11 +112,80 @@ bool take_link(game_t *game, char **file)
 		line = str_to_tab(file[i], " ");
 		if (line [0] != NULL && line[1] == NULL && count_bar(line[0]) == true) {
 			link = str_to_tab(line[0], "-");
+			if (my_strcmp(link[0], link[1]) == 0)
+				return (false);
 			if (check_room_exist(game, link) == false)
 				return (false);
 		}
 	}
 	return (true);
+}
+/*
+int count_malloc_next(room_t *room, char **file)
+{
+	char **line;
+	char **link;
+	int next = 0;
+
+	for (int i = 0; file[i]; i++) {
+		line = str_to_tab(file[i], " ");
+		if (line [0] != NULL && line[1] == NULL && count_bar(line[0]) == true) {
+			link = str_to_tab(line[0], "-");
+			my_strcmp(room->name, link[0]) == 0 ? next++ : 0;
+			my_strcmp(room->name, link[1]) == 0 ? next++ : 0;
+		}
+	}
+		return (next);
+}
+*/
+
+void init_next(game_t *game, char **file)
+{
+	for (size_t i = 0; i < game->nb_room; i++) {
+		game->room[i].nb_next = 0;
+		game->room[i].next = NULL;
+	}
+}
+int find_room(game_t *game, char *room)
+{
+	for (size_t i = 0; i < game->nb_room; i++)
+		if (my_strcmp(game->room[i].name, room) == 0) {
+			return (i);
+		}
+	return (0);
+}
+
+void linked_room(room_t *room1, room_t *room2)
+{
+	room_t **next1 = malloc(sizeof(room_t *) * (room1->nb_next + 1));
+	room_t **next2 = malloc(sizeof(room_t *) * (room2->nb_next + 1));
+
+	for (size_t i = 0; i < room1->nb_next; i++)
+		next1[i] = room1->next[i];
+	next1[room1->nb_next] = room2;
+	for (size_t i = 0; i < room2->nb_next; i++)
+		next2[i] = room2->next[i];
+	next2[room2->nb_next] = room1;
+	free(room1->next);
+	room1->next = next1;
+	free(room2->next);
+	room2->next = next2;
+	room1->nb_next += 1;
+	room2->nb_next += 1;
+}
+
+void make_link(game_t *game, char **file)
+{
+	char **line;
+	char **link;
+
+	for (int i = 0; file[i]; i++) {
+		line = str_to_tab(file[i], " ");
+		if (line [0] != NULL && line[1] == NULL && count_bar(line[0]) == true) {
+			link = str_to_tab(line[0], "-");
+	linked_room(&game->room[find_room(game, link[0])], &game->room[find_room(game, link[1])]);
+		}
+	}
 }
 
 game_t game_create(void)
@@ -133,12 +202,15 @@ game_t game_create(void)
 	take_info(&game, file);
 	if (parsing2(&game) == false)
 		return (game);
-	if (take_link(&game, file) == false)
+	if (parsing3(&game, file) == false)
 		return (game);
-	printf("sname :%s|sx : %d|sy : %d\n", game.start->name, game.start->x, game.start->y);//tmp
-	printf("ename :%s|ex : %d|ey : %d\n", game.end->name, game.end->x, game.end->y);//tmp
-	for (int j = 0; j < count_rooms(file); j++)//tmp
-		printf("nb_room %d|name :%s|x : %d|y : %d\n", count_rooms(file), game.room[j].name, game.room[j].x, game.room[j].y);//tmp
+	init_next(&game, file);
+	make_link(&game, file);
+	//printf("%d\n|next : %s\n", game.start->nb_next, game.room[5].next[1]->name);//tmp
+	//printf("sname :%s|sx : %d|sy : %d\n", game.start->name, game.start->x, game.start->y);//tmp
+	//printf("ename :%s|ex : %d|ey : %d\n", game.end->name, game.end->x, game.end->y);//tmp
+	//for (int j = 0; j < count_rooms(file); j++)//tmp
+	//printf("nb_room %d|name :%s|x : %d|y : %d\n", count_rooms(file), game.room[j].name, game.room[j].x, game.room[j].y);//tmp
 	for (int i = 0; file[i] != NULL; i++) //tmp
 		printf("%s\n", file[i]); //tmp
 	return (game);
