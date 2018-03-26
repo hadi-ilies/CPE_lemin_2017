1c426cb43387dd9efd96a3dea246de8cf0e0c1b6
hadi-ilies.bereksi-reguig <hadi-ilies.bereksi-reguig@epitech.eu>
2018-03-26 16:46:02 +0200

    correct atoi

--- lib/my_atoi.c
+++ lib/my_atoi.c
@@ -14,10 +14,7 @@ int my_atoi(char *str)
 
 	if (str == NULL)
 		return (-1);
-	for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
-		if (str[i] < '0' || str[i] > '9')
-			return (-2);
-	} for (; *str >= '0' && *str <= '9'; (str)++)
+	for (; *str >= '0' && *str <= '9'; (str)++)
 		nb = nb * 10 + (*str - '0');
 	if (str[0] == '-')
 		return (-nb);
